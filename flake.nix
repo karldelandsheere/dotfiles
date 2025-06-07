85dceee46b0d7ff3c4a21c36b8ed31ca8875b336
Karl <karl@delandsheere.be>
2025-06-07 23:01:29 +0200

    Home-manager config ported.

--- flake.nix
+++ flake.nix
@@ -1,14 +1,12 @@
 {
   description = "This is my life now.";
 
-  # https://discourse.nixos.org/t/how-to-set-up-cachix-in-flake-based-nixos-config/31781
-  # use --accept-flake-config when rebuilding
   nixConfig = {
-    extra-substituters = [
+    trusted-substituters = [
       "https://nix-community.cachix.org"
       "https://hyprland.cachix.org"
     ];
-    extra-trusted-public-keys = [
+    trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
     ];
