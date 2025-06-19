38f8c0c2a3f429e6ae11e7aef8a2005f7ed9a6cb
Karl <karl@delandsheere.be>
2025-06-20 00:51:22 +0200

    Added wlogout

--- home-manager/desktop/utils/wlogout/default.nix
+++ home-manager/desktop/utils/wlogout/default.nix
@@ -4,54 +4,56 @@
     programs = {
       wlogout = {
         enable = true;
-        layout = [
-          {
-            label = "lock";
-            action = "swaylock -f";
-            text = "Lock";
-            keybind = "l";
-          }
+        # layout = [
+        #   {
+        #     label = "lock";
+        #     action = "swaylock -f";
+        #     text = "Lock";
+        #     keybind = "l";
+        #   }
 
-          {
-            label = "hibernate";
-            action = "systemctl hibernate";
-            text = "Hibernate";
-            keybind = "h";
-          }
+        #   {
+        #     label = "hibernate";
+        #     action = "systemctl hibernate";
+        #     text = "Hibernate";
+        #     keybind = "h";
+        #   }
           
-          {
-            label = "logout";
-            action = "swaylock -f";
-            text = "Logout";
-            keybind = "o";
-          }
+        #   {
+        #     label = "logout";
+        #     action = "swaylock -f";
+        #     text = "Logout";
+        #     keybind = "o";
+        #   }
 
-          {
-            label = "shutdown";
-            action = "systemctl poweroff";
-            text = "Shutdown";
-            keybind = "s";
-          }
+        #   {
+        #     label = "shutdown";
+        #     action = "systemctl poweroff";
+        #     text = "Shutdown";
+        #     keybind = "s";
+        #   }
 
-          {
-            label = "suspend";
-            action = "systemctl suspend";
-            text = "Suspend";
-            keybind = "s";
-          }
+        #   {
+        #     label = "suspend";
+        #     action = "systemctl suspend";
+        #     text = "Suspend";
+        #     keybind = "s";
+        #   }
 
-          {
-            label = "reboot";
-            action = "systemctl reboot";
-            text = "Reboot";
-            keybind = "r";
-          }
-        ];
+        #   {
+        #     label = "reboot";
+        #     action = "systemctl reboot";
+        #     text = "Reboot";
+        #     keybind = "r";
+        #   }
+        # ];
       };
     };
 
     
     # @todo Find how to not have to type the whole path
     home.file.".config/wlogout".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/home-manager/desktop/utils/wlogout/config";
+    # home.file.".config/wlogout/style.css".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/home-manager/desktop/utils/wlogout/config/style.css";
+    # home.file.".config/wlogout/icons".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/home-manager/desktop/utils/wlogout/config/icons";
   };
 }
