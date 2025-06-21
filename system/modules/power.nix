01582b401926c2229c92dae00e157325af3dea99
Karl <karl@delandsheere.be>
2025-06-21 22:49:23 +0200

    Setting up power management, and hibernation (not ready yet)

--- system/modules/power.nix
+++ system/modules/power.nix
@@ -1,24 +1,53 @@
 { pkgs, ... }:
-
 {
-  # More stuff to come here, later...
-  # If not, I'll put this in the default.nix
-  # ----------------------------------------
+  # Power management
+  # https://nixos.wiki/wiki/Laptop
+  # https://nixos.wiki/wiki/Hibernation
+  # -----------------------------------
 
-  # services.power-profiles.daemon.enable = true;
-
-  # services.powerstation.enable = true;
-  # services.upower.enable = true;
 
   powerManagement = {
     enable = true;
     cpuFreqGovernor = "powersave"; #performance, ondemand, schedutil
   };
 
-  services.logind = {
-    lidSwitch = "suspend";
-    lidSwitchExternalPower = "suspend";
-    lidSwitchDocked = "ignore";
+
+  services = {
+    power-profiles-daemon.enable = true;
+    # services.powerstation.enable = true;
+    # services.upower.enable = true;
+
+    
+    tlp = {
+      enable = true;
+
+      settings = {
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+
+        CPU_MIN_PERF_ON_AC = 0;
+        CPU_MAX_PERF_ON_AC = 100;
+        CPU_MIN_PERF_ON_BAT = 0;
+        CPU_MAX_PERF_ON_BAT = 40;
+
+        # Optional helps save long term battery health
+        # --------------------------------------------
+        START_CHARGE_THRESH_BAT0 = 30; # 30 and below it starts to charge
+        STOP_CHARGE_THRESH_BAT0 = 90; # 90 and above it stops charging
+      };
+    };
+
+
+    logind = {
+      lidSwitch = "suspend";
+      # lidSwitch = "suspend-then-hibernate"; # when hibernate will be implemented
+      lidSwitchExternalPower = "suspend";
+      lidSwitchDocked = "ignore";
+      # powerKey = "hibernate";
+      powerKeyLongPress = "poweroff";
+    };
   };
 
 
