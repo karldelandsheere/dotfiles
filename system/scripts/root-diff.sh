a57427d1e95a8a27c73256181460c5b17b76ace8
Karl <karl@delandsheere.be>
2025-06-25 12:07:44 +0200

    Some side idea

--- system/scripts/root-diff.sh
+++ system/scripts/root-diff.sh
@@ -9,11 +9,11 @@ mount -o subvol=/ /dev/nvme0n1p2 "${_tmp_root}" > /dev/null 2>&1
 
 set -euo pipefail
 
-OLD_TRANSID=$(sudo btrfs subvolume find-new ${_tmp_root}/root-blank 9999999)
+OLD_TRANSID=$(sudo btrfs subvolume find-new "${_tmp_root}/root-blank" 9999999)
 OLD_TRANSID=${OLD_TRANSID#transid marker was }
 
 sudo btrfs subvolume find-new "${_tmp_root}/root" "$OLD_TRANSID" | sed '$d' | cut -f17- -d' ' | sort | uniq | 
-while read path; do
+while read -r path; do
     path="/$path"
     if [ -L "$path" ]; then
         : # The path is a symbolic link, so is probably handled by NixOS already
