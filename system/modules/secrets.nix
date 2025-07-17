cad8f6be5df77dfd3c54846311b465ca6c9214cc
Karl <karl@delandsheere.be>
2025-07-17 12:15:31 +0200

    Adding secrets management

--- system/modules/secrets.nix
+++ system/modules/secrets.nix
@@ -1,22 +1,22 @@
 {
-  # age = {
-  #   secrets = {
-  #     # Hosts
-  #     q3dm10 = {
-  #       file  = ../../secrets/q3dm10.age;
-  #       group = "wheel";
-  #       mode  = "0440";
-  #     };
+  age = {
+    secrets = {
+      # Hosts
+      q3dm10 = {
+        file  = ../../secrets/q3dm10.age;
+        group = "wheel";
+        mode  = "0440";
+      };
 
-  #     # Users
-  #     unnamedplayer = {
-  #       file = ../../secrets/unnamedplayer.age;
-  #       owner = "unnamedplayer";
-  #     };
-  #   };
-  #   # wg-DE = {
-  #   #   file = ../../secrets/wg-DE.age;
-  #   #   path = "/etc/wireguard/DE.conf";
-  #   # };
-  # };
+      # Users
+      unnamedplayer = {
+        file = ../../secrets/unnamedplayer.age;
+        owner = "unnamedplayer";
+      };
+    };
+    # wg-DE = {
+    #   file = ../../secrets/wg-DE.age;
+    #   path = "/etc/wireguard/DE.conf";
+    # };
+  };
 }
