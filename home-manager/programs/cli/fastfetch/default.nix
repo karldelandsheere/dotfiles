5ec21cf0b725eb6245a6fd7bb010a881c7b9e9d9
Karl <karl@delandsheere.be>
2025-07-27 14:29:41 +0200

    Added some fuzzel stuff and other random stuff

---
+++ home-manager/programs/cli/fastfetch/default.nix
@@ -0,0 +1,12 @@
+# Helix
+# -----
+{ config, osConfig, pkgs, ... }:
+{
+  config = {
+    home = {
+      packages = [ pkgs.fastfetch ];
+      file.".config/fastfetch".source =
+        config.lib.file.mkOutOfStoreSymlink "${osConfig.nouveauxParadigmes.dotfiles}/home-manager/programs/cli/fastfetch/config";
+    };
+  };
+}
