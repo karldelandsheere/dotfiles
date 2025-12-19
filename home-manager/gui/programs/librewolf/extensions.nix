f5e36abe9f36a8ae8b43db3e46274f1690a9ffc3
Karl <karl@delandsheere.be>
2025-12-19 01:46:38 +0100

    Switching to Librewolf

--- home-manager/gui/programs/librewolf/extensions.nix
+++ home-manager/gui/programs/librewolf/extensions.nix
@@ -6,7 +6,7 @@
     id = "bitwarden-password-manager";
     installation_mode = "force_installed";
     private_browsing = true;
-    settings = { };
+    settings = {};
     permissions = [
       "activeTab"
       "clipboardWrite"
@@ -26,108 +26,68 @@
     ];
   };
   
-  "CanvasBlocker@kkapsner.de" = {
-    id = "canvasblocker";
-    installation_mode = "normal_installed";
-    private_browsing = true;
-    settings = { };
-    permissions = [
-      "activeTab"
-      "tabs"
-      "storage"
-      "<all_urls>"
-    ];
-  };
+  # "CanvasBlocker@kkapsner.de" = {
+  #   id = "canvasblocker";
+  #   installation_mode = "normal_installed";
+  #   private_browsing = true;
+  #   settings = {};
+  #   permissions = [
+  #     "activeTab"
+  #     "tabs"
+  #     "storage"
+  #     "<all_urls>"
+  #   ];
+  # };
   
-  "{74145f27-f039-47ce-a470-a662b129930a}" = {
-    id = "clearurls";
-    installation_mode = "normal_installed";
-    private_browsing = true;
-    settings = { };
-    permissions = [
-      "activeTab"
-      "tabs"
-      "storage"
-      "<all_urls>"
-    ];
-  };
+  # "{74145f27-f039-47ce-a470-a662b129930a}" = {
+  #   id = "clearurls";
+  #   installation_mode = "normal_installed";
+  #   private_browsing = true;
+  #   settings = { };
+  #   permissions = [
+  #     "activeTab"
+  #     "tabs"
+  #     "storage"
+  #     "<all_urls>"
+  #   ];
+  # };
   
-  "gdpr@cavi.au.dk" = {
-    id = "consent-o-matic";
-    installation_mode = "normal_installed";
-    private_browsing = true;
-    settings = { };
-    permissions = [
-      "activeTab"
-      "tabs"
-      "storage"
-      "<all_urls>"
-    ];
-  };
-  
-  "jid1-MnnxcxisBPnSXQ@jetpack" = {
-    id = "privacy-badger17";
-    installation_mode = "normal_installed";
-    private_browsing = true;
-    settings = {
-      selectedFilterLists = [
-        "user-filters"
-        "ublock-filters"
-        "ublock-badware"
-        "ublock-privacy"
-        "ublock-unbreak"
-        "ublock-quick-fixes"
-        "easylist"
-        "easyprivacy"
-        "urlhaus-1"
-        "plowe-0"
-      ];
-    };
-    permissions = [
-      "activeTab"
-      "tabs"
-      "storage"
-      "<all_urls>"
-    ];
-  };
-  
-  "uBlock0@raymondhill.net" = {
-    id = "ublock-origin";
-    installation_mode = "normal_installed";
-    private_browsing = true;
-    settings = {
-      selectedFilterLists = [
-        "user-filters"
-        "ublock-filters"
-        "ublock-badware"
-        "ublock-privacy"
-        "ublock-quick-fixes"
-        "ublock-unbreak"
-        "easylist"
-        "easyprivacy"
-        "adguard-spyware"
-        "adguard-spyware-url"
-        "block-lan"
-        "urlhaus-1"
-        "plowe-0"
-        "dpollock-0"
-        "fanboy-cookiemonster"
-        "ublock-cookies-easylist"
-        "adguard-cookies"
-        "ublock-cookies-adguard"
-        "easylist-chat"
-        "easylist-newsletters"
-        "easylist-notifications"
-        "easylist-annoyances"
-        "adguard-mobile-app-banners"
-        "adguard-other-annoyances"
-      ];
-    };
-    permissions = [
-      "activeTab"
-      "tabs"
-      "storage"
-      "<all_urls>"
-    ];
-  };
+  # "gdpr@cavi.au.dk" = {
+  #   id = "consent-o-matic";
+  #   installation_mode = "normal_installed";
+  #   private_browsing = true;
+  #   settings = { };
+  #   permissions = [
+  #     "activeTab"
+  #     "tabs"
+  #     "storage"
+  #     "<all_urls>"
+  #   ];
+  # };
+
+  # "jid1-MnnxcxisBPnSXQ@jetpack" = {
+  #   id = "privacy-badger17";
+  #   installation_mode = "normal_installed";
+  #   private_browsing = true;
+  #   settings = {
+  #     selectedFilterLists = [
+  #       "user-filters"
+  #       "ublock-filters"
+  #       "ublock-badware"
+  #       "ublock-privacy"
+  #       "ublock-unbreak"
+  #       "ublock-quick-fixes"
+  #       "easylist"
+  #       "easyprivacy"
+  #       "urlhaus-1"
+  #       "plowe-0"
+  #     ];
+  #   };
+  #   permissions = [
+  #     "activeTab"
+  #     "tabs"
+  #     "storage"
+  #     "<all_urls>"
+  #   ];
+  # };
 }
