532dff6a98655f6af6783d5a7f840daba1acb467
Karl <karl@delandsheere.be>
2025-12-27 21:49:20 +0100

    Moving secret management to home-manager and into the user's directory for better organization

--- users/unnamedplayer/modules/secrets.nix
+++ users/unnamedplayer/modules/secrets.nix
@@ -2,14 +2,12 @@
 {
   config = {
     age = {
-      identityPaths = [ "~/.ssh/id_ed25519" ];
-
       secrets = {
-        "auth/bw".file = ../../secrets/auth/bw.age;
-        "auth/mail/karl_at_delandsheere_be".file = ../../secrets/auth/mail/karl_at_delandsheere_be.age;
-        "auth/mail/karl_at_nouveaux-paradigmes_be".file = ../../secrets/auth/mail/karl_at_nouveaux-paradigmes_be.age;
-        "auth/matrix/dimeritium".file = ../../secrets/auth/matrix/dimeritium.age;
-        "auth/tailscale/dimeritium".file = ../../secrets/auth/tailscale/dimeritium.age;
+        "auth/bw".file = ../secrets/auth/bw.age;
+        "auth/mail/karl_at_delandsheere_be".file = ../secrets/auth/mail/karl_at_delandsheere_be.age;
+        "auth/mail/karl_at_nouveaux-paradigmes_be".file = ../secrets/auth/mail/karl_at_nouveaux-paradigmes_be.age;
+        "auth/matrix/dimeritium".file = ../secrets/auth/matrix/dimeritium.age;
+        "auth/tailscale/dimeritium".file = ../secrets/auth/tailscale/dimeritium.age;
       };
     };
   };
